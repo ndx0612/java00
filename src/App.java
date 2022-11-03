af548b4d5080fb79acba25fa1cf537b16031fc9b
ndx2527 <1378362527@qq.com>
2022-11-03 19:58:05 +0800

    fix：项目初始化

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // 输出 Hello World
+        System.out.println("1"); // 输出 1
+        System.out.println("2"); // 输出 2
+        System.out.println("3"); // 输出 3
+    }
+}
