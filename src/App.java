f7af0a2dcce49670ceb874ecff32f9a791b26434
ndx2527 <1378362527@qq.com>
2022-11-10 09:07:19 +0800

    fix：包创建和导入

--- src/App.java
+++ src/App.java
@@ -1,35 +1,7 @@
-import java.util.Arrays;
+import ming.Person;
 
 public class App {
     public static void main(String[] args) {
-        Person ming = new Person();
-        ming.setName("小明");
-        ming.setAge(20);
-        System.out.println(ming.getName());
-        System.out.println(ming.getAge());
+        System.out.println(Person.score);
     }
 }
-
-class Person {
-    private String name;
-    private int age;
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return this.age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-
-}
-
